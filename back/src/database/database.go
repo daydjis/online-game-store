a272d543ba3e3a91d09dd64fbd48d64c80a3a549
Evgeniy Folomeev <e.folomeev@vk.team>
2022-12-04 20:19:48 +0300

    added backed

---
+++ back/src/database/database.go
@@ -0,0 +1,75 @@
+package database
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+)
+
+const uri = "mongodb+srv://geniy_folomeev:26051978aA%21@gamestore.dsm2ow5.mongodb.net/?retryWrites=true&w=majority"
+
+type Game struct {
+	ID          primitive.ObjectID `bson:"_id"`
+	Title       string
+	Description string
+	Price       int64
+	Genres      []string
+	Image       string
+	Video       string
+}
+
+var ctx = context.TODO()
+var opts = options.Client().ApplyURI(uri)
+
+func HealthCheck() *mongo.Client {
+	// Create a new client and connect to the server
+	client, err := mongo.Connect(ctx, opts)
+	if err != nil {
+		panic(err)
+	}
+	defer func() {
+		if err = client.Disconnect(context.TODO()); err != nil {
+			panic(err)
+		}
+	}()
+	// Ping the primary
+	if err := client.Ping(context.TODO(), readpref.Primary()); err != nil {
+		panic(err)
+	}
+	fmt.Println("Successfully connected and pinged.")
+	return client
+}
+
+func GetGames() ([]Game){
+	var result []Game
+	findOptions := options.Find()
+	
+	client, _ := mongo.Connect(ctx, opts)
+	col := client.Database("games").Collection("games_collection")
+	cur, err := col.Find(ctx, bson.D{{}}, findOptions)
+    if err !=nil {
+        log.Fatal(err)
+    }
+	for cur.Next(ctx) {
+        var elem Game
+        err := cur.Decode(&elem)
+        if err != nil {
+            log.Fatal(err)
+        }
+
+        result = append(result, elem)
+
+    }
+	defer func() {
+		if err = client.Disconnect(ctx); err != nil {
+			panic(err)
+		}
+	}()
+	return result
+}

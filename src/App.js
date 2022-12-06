10fc557145a4f131762a5ebff014466568cc21e8
daydjis <100149743+daydjis@users.noreply.github.com>
2022-12-06 17:57:57 +0400

    add game page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import HomePage from './pages/home-page'
+import GamePage from './pages/game-page'
 import Header from './components/header'
 import { Provider } from 'react-redux'
 import { store } from './redux'
@@ -11,9 +12,8 @@ function App() {
         <div className="App">
           <Header />
           <Switch>
-            <Route path="/">
-              <HomePage />
-            </Route>
+            <Route exact path="/" component={HomePage} />
+            <Route exact path="/app/:title" component={GamePage} />
           </Switch>
         </div>
       </Router>

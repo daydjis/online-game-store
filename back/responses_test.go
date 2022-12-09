dfe0c3c78f33e54bb3d4571a68be44d8a88bd9d4
Evgeniy Folomeev <e.folomeev@vk.team>
2022-12-09 20:40:47 +0300

    Накинул еще юнит тестов

--- back/responses_test.go
+++ back/responses_test.go
@@ -2,35 +2,55 @@ package main
 
 import (
 	"errors"
-	"fmt"
 	"testing"
 )
 
-func TestMakeResponseForPostPositive(t *testing.T) {
-	gameId := "randomId"
-	expResponse := fmt.Sprintf("{\"Result\":\"Game was created successfully\", \"id\": \"%s\"}", gameId)
-	expStatus := 200
-
-	response, status := MakeResponseForPost(gameId, nil)
-	if response != expResponse {
-		t.Errorf("Expected response: %s, Got: %s", expResponse, response)
-	}
-	if status != expStatus {
-		t.Errorf("Expected status: %d, Got: %d", expStatus, status)
-	}
+type MakeResponseForPostParameters struct {
+	gameID           string
+	err              interface{ Error() string }
+	expectedResponse string
+	expectedStatus   int
 }
 
-func TestMakeResponseForPostNegative(t *testing.T) {
-	gameId := "randomId2"
-	err := errors.New("such a horrible error")
-	expResponse := fmt.Sprintf("{\"Result\":\"Game was not created,\"Error\": \"%s\"}", err)
-	expStatus := 500
+var MakeResponseForPostValues = []MakeResponseForPostParameters{
+	{"randomId", nil, "{\"Result\":\"Game was created successfully\", \"id\": \"randomId\"}", 200},
+	{"randomId2", errors.New("such a horrible error"), "{\"Result\":\"Game was not created,\"Error\": \"such a horrible error\"}", 500},
+}
 
-	response, status := MakeResponseForPost(gameId, err)
-	if response != expResponse {
-		t.Errorf("Expected response: %s, Got: %s", expResponse, response)
+func TestMakeResponseForPost(t *testing.T) {
+	for _, arg := range MakeResponseForPostValues {
+		response, status := MakeResponseForPost(arg.gameID, arg.err)
+		if response != arg.expectedResponse {
+			t.Errorf("Expected response: %s, Got: %s", arg.expectedResponse, response)
+		}
+		if status != arg.expectedStatus {
+			t.Errorf("Expected status: %d, Got: %d", arg.expectedStatus, status)
+		}
 	}
-	if status != expStatus {
-		t.Errorf("Expected status: %d, Got: %d", expStatus, status)
+
+}
+
+type MakeResponseForDeleteParameters struct {
+	deletedCount     int64
+	err              interface{ Error() string }
+	expectedResponse string
+	expectedStatus   int
+}
+
+var MakeResponseForDeleteValues = []MakeResponseForDeleteParameters{
+	{0, errors.New("such a horrible error"), "{\"Result\":\"Game was not deleted,\"Error\": \"such a horrible error\"}", 500},
+	{0, nil, "{\"Result\":\"Game was not deleted,\"Error\": \"Wrong game id\"}", 404},
+}
+
+func TestMakeResponseForDeleteErr(t *testing.T) {
+	for _, arg := range MakeResponseForDeleteValues {
+		response, status := MakeResponseForDelete(arg.deletedCount, arg.err)
+		if response != arg.expectedResponse {
+			t.Errorf("Expected response: %s, Got: %s", arg.expectedResponse, response)
+		}
+		if status != arg.expectedStatus {
+			t.Errorf("Expected status: %d, Got: %d", arg.expectedStatus, status)
+		}
+
 	}
 }

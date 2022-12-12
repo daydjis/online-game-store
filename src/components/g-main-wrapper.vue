adafaba1606e3ebe488922100e9972a54b80a599
daydjis <100149743+daydjis@users.noreply.github.com>
2022-12-12 22:49:53 +0400

    Добавил корзину и начал настраивать руты

--- src/components/g-main-wrapper.vue
+++ src/components/g-main-wrapper.vue
@@ -5,13 +5,26 @@
     <div class="home-page_wrapper shadow p-3 mb-5 bg-body rounded">
         <g-game-catalog />
     </div>
+    <div class="home-page_wrapper shadow p-3 mb-5 bg-body rounded">
+        <g-cart v-if="CART.length" :itemsInCart="CART.length" :cartItemsInfo="CART" />
+    </div>
+
 </template>
 
 <script>
+
 import GGameCatalog from './g-game-catalog.vue'
 import gHeader from './g-header.vue'
+import gCart from './g-cart.vue'
+import { mapGetters } from 'vuex';
+
 export default {
-    components: { gHeader, GGameCatalog },
+    components: { gHeader, GGameCatalog, gCart },
+    computed: {
+        ...mapGetters([
+            'CART'
+        ])
+    }
 }
 </script>
 
